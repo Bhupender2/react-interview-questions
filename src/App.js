03fb01f1fa05781a35d4833f617303284f6f927b
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-06 18:35:53 +0530

    put the timer in different folder

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import Surprise from "./Surprise";
+import Timer from "./TimerQuestion/Timer";
 
 function App() {
-  const [time, setTime] = useState(100);
-  const min = Math.floor(time / 60);
-  const sec = time % 60;
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime((time) => {
-        if (time > 0) return time - 1;
-        else {
-          clearInterval(id);
-          return 0;
-        }
-      });
-    }, 1000);
-    return function () {
-      clearInterval(id);
-    };
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        {time > 0 && (
-          <div
-            style={{
-              height: "200px",
-              width: "400px",
-              backgroundColor: "white",
-              color: "black",
-              borderRadius: "10px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontSize: "90px",
-            }}
-          >
-            {min > 10 ? "" : "0"}
-            {min}:{sec < 10 ? "0" : ""}
-            {sec}
-          </div>
-        )}
-        {(time === 0 || time < 0) && <Surprise />}{" "}
-        {/*conditional rendering done*/}
-      </header>
-    </div>
+    <>
+      <Timer />
+    </>
   );
 }
 
